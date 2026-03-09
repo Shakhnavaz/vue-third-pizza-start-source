d6ea0a2be8f76c496ae835ec60c0538461f4d184
Shakhnavaz <shakhnavaz@gmail.com>
2026-03-09 13:48:01 +0300

    task 1

---
+++ frontend/src/layouts/AppHeader.vue
@@ -0,0 +1,26 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <header class="header">
+    <div class="header__logo">
+      <a href="index.html" class="logo">
+        <img src="img/logo.svg" alt="V!U!E! Pizza logo" width="90" height="40">
+      </a>
+    </div>
+    <div class="header__cart">
+      <a href="cart.html">0 ₽</a>
+    </div>
+    <div class="header__user">
+      <a href="#" class="header__login"><span>Войти</span></a>
+    </div>
+</header>
+</body>
+
+
+</html>
+
+
